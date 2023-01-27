860bad7e45d868b4bb1218ee557126519d49f2e2
nni-odoo <nni@odoo.com>
2023-01-27 16:33:33 +0800

    Module added

---
+++ training/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Training',
+    'version': '1.0',
+    'summary': 'Training',
+    'description': """
+        Training
+    """,
+    'depends': ['base'],
+    'data': [
+    ],
+    'license': 'LGPL-3',
+}
